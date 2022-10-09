676cbeab4decd9dbbd582a333faa181127623dde
abrusu-collins <snillochemingson@gmail.com>
2022-10-09 12:11:49 +0000

    hero works

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,21 @@
 function Hero() {
-    return (
-<div className="main-hero">
-<div className="hero">
-            <p className="tag-line">We create world class digital products that our clients fall in love with.</p>
-            <p>DevTech is a sofware development agency in Accra, Ghana. We help you own quality and affordable digital products easily. We develop Websites, Android Apps, and Desktop Apps</p>
-           <p>Let's discuss your project</p>
-           <a href=" ">Book a Call</a> 
-        </div>
-</div>
-      );
+  return (
+    <div className="main-hero">
+      <div className="hero">
+        <p className="tag-line">
+          We create world class digital products that our clients fall in love
+          with.
+        </p>
+        <p>
+          DevTech is a sofware development agency in Accra, Ghana. We help you
+          own quality and affordable digital products easily. We develop
+          Websites, Android Apps, and Desktop Apps. We also do bug fixing and UI/UX improvements.
+        </p>
+        <p>Let's discuss your project</p>
+        <a href=" ">Book a Call</a>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
