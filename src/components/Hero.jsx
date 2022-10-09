1b581cd26a8534471c73eb1e4fc4b57306d8871c
abrusu-collins <snillochemingson@gmail.com>
2022-10-09 12:07:05 +0000

    hero works

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,13 @@
 function Hero() {
     return (
-        <div className="hero">
-            <p className="tagLine">We create world class digital products that our clients fall in love with.</p>
-            <p></p>
+<div className="main-hero">
+<div className="hero">
+            <p className="tag-line">We create world class digital products that our clients fall in love with.</p>
+            <p>DevTech is a sofware development agency in Accra, Ghana. We help you own quality and affordable digital products easily. We develop Websites, Android Apps, and Desktop Apps</p>
            <p>Let's discuss your project</p>
-           <a href=" ">Book a Call</a>
+           <a href=" ">Book a Call</a> 
         </div>
+</div>
       );
 }
 
