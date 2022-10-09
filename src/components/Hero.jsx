52b2fde6ddcae8361e102de37dbb667970dedf76
abrusu-collins <snillochemingson@gmail.com>
2022-10-09 16:42:54 +0000

    nice

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
+import heroimg from "../img/hero-banner.gif";
 function Hero() {
   return (
     <div className="main-hero">
       <div className="hero">
-        <p className="tag-line">
-          We create world class digital products that our clients fall in love
-          with.
+      <div className="hero-left">
+      <p className="tag-line">
+          We create world class digital products.
         </p>
         <p className="descibe">
           DevTech is a sofware development agency in Accra, Ghana. We help you
@@ -15,6 +16,11 @@ function Hero() {
         </p>
         <p className="discuss">Let's discuss your project.</p>
         <a href=" ">Talk To Us Now</a>
+      </div>
+      <div className="hero-right">
+      <img src= {heroimg} alt="" />
+
+      </div>
       </div>
     </div>
   );
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,6 +2,14 @@ function Services() {
     return ( 
         <div className="services">  
         <p className="title">OUR SERVICES</p>
+        <div className="listofservices">
+            <div className="web">
+                <p className="number">01</p>
+                <p>Website Development</p>
+                <img src="" alt="" />
+
+            </div>
+        </div>
         </div>
      );
 }
