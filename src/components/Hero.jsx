2f35d0ea4e7d0d940bef3ca7c47036bd058aa635
abrusu-collins <snillochemingson@gmail.com>
2022-10-09 16:43:18 +0000

    formatted

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,24 +3,21 @@ function Hero() {
   return (
     <div className="main-hero">
       <div className="hero">
-      <div className="hero-left">
-      <p className="tag-line">
-          We create world class digital products.
-        </p>
-        <p className="descibe">
-          DevTech is a sofware development agency in Accra, Ghana. We help you
-          own quality and affordable digital products easily. We develop
-          <span> Websites</span>, <span>Android Apps</span>, and
-          <span> Desktop Apps</span>. We also do <span>bug fixing</span> and <span>UI/UX
-          improvements</span>.
-        </p>
-        <p className="discuss">Let's discuss your project.</p>
-        <a href=" ">Talk To Us Now</a>
-      </div>
-      <div className="hero-right">
-      <img src= {heroimg} alt="" />
-
-      </div>
+        <div className="hero-left">
+          <p className="tag-line">We create world class digital products.</p>
+          <p className="descibe">
+            DevTech is a sofware development agency in Accra, Ghana. We help you
+            own quality and affordable digital products easily. We develop
+            <span> Websites</span>, <span>Android Apps</span>, and
+            <span> Desktop Apps</span>. We also do <span>bug fixing</span> and{" "}
+            <span>UI/UX improvements</span>.
+          </p>
+          <p className="discuss">Let's discuss your project.</p>
+          <a href=" ">Talk To Us Now</a>
+        </div>
+        <div className="hero-right">
+          <img src={heroimg} alt="heroimg" />
+        </div>
       </div>
     </div>
   );
