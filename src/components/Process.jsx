2cb46dfaf344ff42f21372e958de77bd924f111c
abrusu-collins <snillochemingson@gmail.com>
2022-10-10 19:41:55 +0000

    nice

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -5,27 +5,38 @@ function Process() {
       <div className="processes">
         <div>
           <p className="numbered">
-            <span>01</span> Specification
+            <span>01</span>Specification
           </p>
-          <p> </p>
+          <p>
+            Send us a mail that contains exactly what you want. When you click
+            on the "Talk To Us Now" button,
+          </p>
         </div>
         <div>
           <p className="numbered">
             <span>02</span>Get a quote
           </p>
-          <p> </p>
+          <p>
+            Receive the cost of the service.The cost varies and solely depends
+            on what you want.
+          </p>
         </div>
         <div>
           <p className="numbered">
             <span>03</span>Your Approval
           </p>
-          <p> </p>
+          <p>
+            You then tell us that you've agreed to pay the cost for us to
+            proceed.
+          </p>
         </div>
         <div>
           <p className="numbered">
             <span>04</span>Work Starts
           </p>
-          <p> </p>
+          <p>
+            Our team then starts working on the project as soon as possible.{" "}
+          </p>
         </div>
       </div>
     </div>
