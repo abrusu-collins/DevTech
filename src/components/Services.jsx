e89380ad0e3d7a6e89643f1eefb8f29c259f5ba9
abrusu-collins <snillochemingson@gmail.com>
2022-10-10 16:58:35 +0000

    more work

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,19 @@
-
-
+import programmerimg from "../img/programmer.jpg";
 function Services() {
   return (
     <div className="services">
       <p className="title">WHAT WE DO</p>
+      <div className="main-services">
+        <img src={programmerimg} alt="programmerimg" />
+        <div className="list-of-services">
+          <div>Website Design</div>
+          <div>Website Design</div>
+          <div>Website Design</div>
+          <div>Website Design</div>
+          <div>Website Design</div>
+
+        </div>
+      </div>
     </div>
   );
 }
