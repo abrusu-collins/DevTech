095844fefb1abef79bd56b39eaac8dd6a9833a95
abrusu-collins <snillochemingson@gmail.com>
2022-10-10 14:04:02 +0000

    font awesome

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -1,7 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 function Why() {
     return ( 
         <div className="why">
             <p className="title">WHY CHOOOSE US</p>
+            <div className="reasons">
+                <div className="fast">
+
+                </div>
+            </div>
         </div>
      );
 }
