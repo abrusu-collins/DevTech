2fe3a869982c46c807583dbaf842d4a3b1a3c806
abrusu-collins <snillochemingson@gmail.com>
2022-10-10 17:07:41 +0000

    God Settings

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,11 +6,11 @@ function Services() {
       <div className="main-services">
         <img src={programmerimg} alt="programmerimg" />
         <div className="list-of-services">
-          <div>Website Design</div>
-          <div>Website Design</div>
-          <div>Website Design</div>
-          <div>Website Design</div>
-          <div>Website Design</div>
+          <div>Website Development</div>
+          <div>Android App Development</div>
+          <div>Desktop App Development</div>
+          <div>Bug Fixing</div>
+          <div>UI/UX Improvement</div>
 
         </div>
       </div>
