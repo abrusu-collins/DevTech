852a8812b1a786c24f56a3985b742c482348853d
abrusu-collins <snillochemingson@gmail.com>
2022-10-10 19:27:35 +0000

    more work

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -1,9 +1,35 @@
 function Process() {
-    return ( 
-        <div className="process">
-            <p className="title">OUR PROCESS</p>
+  return (
+    <div className="process">
+      <p className="title">OUR PROCESS</p>
+      <div className="processes">
+        <div>
+          <p className="numbered">
+            <span>01</span> Specification
+          </p>
+          <p> </p>
         </div>
-     );
+        <div>
+          <p className="numbered">
+            <span>02</span>Get a quote
+          </p>
+          <p> </p>
+        </div>
+        <div>
+          <p className="numbered">
+            <span>03</span>Your Approval
+          </p>
+          <p> </p>
+        </div>
+        <div>
+          <p className="numbered">
+            <span>04</span>Work Starts
+          </p>
+          <p> </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Process;
