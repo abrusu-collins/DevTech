e1db0663ced80ccc1876ee5f5ad5485d42ca1b1c
abrusu-collins <snillochemingson@gmail.com>
2022-10-10 14:08:48 +0000

    reasons

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faBolt } from '@fortawesome/free-solid-svg-icons';
 
 function Why() {
     return ( 
@@ -7,7 +7,39 @@ function Why() {
             <p className="title">WHY CHOOOSE US</p>
             <div className="reasons">
                 <div className="fast">
+                <FontAwesomeIcon icon={faBolt} color= "#0da34e" />
+                <p className="reason-big">Super Fast Delivery</p>
+                <p className='explain'></p>
+                </div>
 
+                <div className="fast">
+                <FontAwesomeIcon icon={faBolt} color= "#0da34e" />
+                <p className="reason-big">Super Fast Delivery</p>
+                <p className='explain'></p>
+                </div>
+
+                <div className="fast">
+                <FontAwesomeIcon icon={faBolt} color= "#0da34e" />
+                <p className="reason-big">Super Fast Delivery</p>
+                <p className='explain'></p>
+                </div>
+
+                <div className="fast">
+                <FontAwesomeIcon icon={faBolt} color= "#0da34e" />
+                <p className="reason-big">Super Fast Delivery</p>
+                <p className='explain'></p>
+                </div>
+
+                <div className="fast">
+                <FontAwesomeIcon icon={faBolt} color= "#0da34e" />
+                <p className="reason-big">Super Fast Delivery</p>
+                <p className='explain'></p>
+                </div>
+
+                <div className="fast">
+                <FontAwesomeIcon icon={faBolt} color= "#0da34e" />
+                <p className="reason-big">Super Fast Delivery</p>
+                <p className='explain'></p>
                 </div>
             </div>
         </div>
