88bbcedd023fbc4b3732a7cfc7f0c8bd963d4b06
abrusu-collins <snillochemingson@gmail.com>
2022-10-10 19:21:09 +0000

    our process

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Why from "./components/Why";
 import Works from "./components/Works";
+import Process from "./components/Process";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Hero />
       <Services />
       <Why />
-      <Works/>
+      <Works />
+      <Process />
     </>
   );
 }
---
+++ src/components/Process.jsx
@@ -0,0 +1,9 @@
+function Process() {
+    return ( 
+        <div className="process">
+            <p className="title">OUR PROCESS</p>
+        </div>
+     );
+}
+
+export default Process;
