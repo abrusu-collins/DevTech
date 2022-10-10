45dc630cd4bb32815cf035f2cae3ab249340650d
abrusu-collins <snillochemingson@gmail.com>
2022-10-10 20:01:57 +0000

    making sense

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -4,36 +4,32 @@ function Process() {
       <p className="title">OUR PROCESS</p>
       <div className="processes">
         <div>
-          <p className="numbered">
-            <span>01</span>Specification
-          </p>
+          <p className="numbered">01</p>
+          <p className="process-title">Specification</p>
           <p>
             Send us a mail that contains exactly what you want. When you click
             on the "Talk To Us Now" button,
           </p>
         </div>
         <div className="back">
-          <p className="numbered">
-            <span>02</span>Get a quote
-          </p>
+          <p className="numbered">02</p>
+          <p className="process-title">Get a quote</p>
           <p>
             Receive the cost of the service.The cost varies and solely depends
             on what you want.
           </p>
         </div>
         <div className="back">
-          <p className="numbered">
-            <span>03</span>Your Approval
-          </p>
+          <p className="numbered">03</p>
+          <p className="process-title">Your Approval</p>
           <p>
             You then tell us that you've agreed to pay the cost for us to
             proceed.
           </p>
         </div>
         <div>
-          <p className="numbered">
-            <span>04</span>Work Starts
-          </p>
+          <p className="numbered">04</p>
+          <p className="process-title">Work Starts</p>
           <p>
             Our team then starts working on the project as soon as possible.{" "}
           </p>
