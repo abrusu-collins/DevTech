df3c8ca5a8393fafa0a65fa716254c9d3dfb05f5
abrusu-collins <snillochemingson@gmail.com>
2022-10-24 23:27:24 +0000

    cool work

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,11 +1,26 @@
+import blackogif from "../img/blacko.gif";
+import hotelvivigif from "../img/hotel-vivi.gif";
+import edemgif from "../img/edem.gif";
+
+
+
 function Works() {
     return (
         <div className="works">
             <p className="title">FEATURED WORK</p>
             <div className="work_gifs">
-                <div className="blacko"></div>
-                <div className="hotel-vivi"></div>
-                <div className="edem"></div>
+                <div className="blacko">
+                    <div className="gif">
+                        <img src={blackogif} alt="blackogif" />
+                    </div>
+                    <div className="about"></div>
+                </div>
+                <div className="hotel-vivi">
+                    <img src={hotelvivigif} alt="" />
+                </div>
+                <div className="edem">
+                    <img src={edemgif} alt="edemgif" />
+                </div>
             </div>
         </div>
       );
