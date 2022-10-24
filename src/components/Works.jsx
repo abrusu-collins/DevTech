14b23f5e886092870f02e0858da4a20905f6129c
abrusu-collins <snillochemingson@gmail.com>
2022-10-24 23:34:49 +0000

    nice work

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,28 +2,28 @@ import blackogif from "../img/blacko.gif";
 import hotelvivigif from "../img/hotel-vivi.gif";
 import edemgif from "../img/edem.gif";
 
-
-
 function Works() {
-    return (
-        <div className="works">
-            <p className="title">FEATURED WORK</p>
-            <div className="work_gifs">
-                <div className="blacko">
-                    <div className="gif">
-                        <img src={blackogif} alt="blackogif" />
-                    </div>
-                    <div className="about"></div>
-                </div>
-                <div className="hotel-vivi">
-                    <img src={hotelvivigif} alt="" />
-                </div>
-                <div className="edem">
-                    <img src={edemgif} alt="edemgif" />
-                </div>
-            </div>
+  return (
+    <div className="works">
+      <p className="works_title">FEATURED WORK</p>
+      <div className="flex2_gifs">
+        <div className="blacko">
+          <div className="gif">
+            <img src={blackogif} alt="blackogif" />
+          </div>
+          <div className="about"></div>
         </div>
-      );
+        <div className="hotel-vivi">
+          <img src={hotelvivigif} alt="hotelvivigif" />
+        </div>
+      </div>
+      <div className="edem_gif">
+        <div className="edem">
+          <img src={edemgif} alt="edemgif" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Works;
