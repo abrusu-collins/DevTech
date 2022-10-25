77b3e814b80db31084fe378f41daf5f078364cab
abrusu-collins <snillochemingson@gmail.com>
2022-10-25 09:58:53 +0000

    nice one

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,17 +1,14 @@
-import programmerimg from "../img/programmer.jpg";
 function Services() {
   return (
     <div className="services">
       <p className="title">WHAT WE DO</p>
       <div className="main-services">
-        <img src={programmerimg} alt="programmerimg" />
         <div className="list-of-services">
-          <div>Website Development</div>
-          <div>Android App Development</div>
-          <div>Desktop App Development</div>
-          <div>Bug Fixing</div>
-          <div>UI/UX Improvement</div>
-
+          <div className="web">Website Development</div>
+          <div className="android">Android App Development</div>
+          <div className="desktop">Desktop App Development</div>
+          <div className="bug">Bug Fixing</div>
+          <div className="uiux">UI/UX Improvement</div>
         </div>
       </div>
     </div>
