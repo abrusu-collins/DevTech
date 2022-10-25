8c632a5ed6c195dbc6105e07ce890e0819ef47eb
abrusu-collins <snillochemingson@gmail.com>
2022-10-25 00:08:20 +0000

    nice one

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -12,23 +12,23 @@ function Works() {
             <img src={blackogif} alt="blackogif" />
           </div>
           <div className="about">
-            <p>Website for Blacko</p>
+            <p>Website for Blacko(Musician)</p>
             <a href="">Visit Website</a>
           </div>
         </div>
-        <div className="hotel-vivi">
-          <img src={hotelvivigif} alt="hotelvivigif" />
+        <div className="edem">
+          <img src={edemgif} alt="edemgif" />
           <div className="about">
-            <p>Website for Blacko</p>
+            <p>Website for Edem Creatives(Graphic Designer)</p>
             <a href="">Visit Website</a>
           </div>
         </div>
       </div>
-      <div className="edem_gif">
-        <div className="edem">
-          <img src={edemgif} alt="edemgif" />
+      <div className="hotel_gif">
+        <div className="hotel-vivi">
+          <img src={hotelvivigif} alt="hotelvivigif" />
           <div className="about">
-            <p>Website for Blacko</p>
+            <p>Website for Hotelvivi(Hotel)</p>
             <a href="">Visit Website</a>
           </div>
         </div>
