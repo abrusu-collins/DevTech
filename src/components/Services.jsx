d07d892a0797ef5f8203593a109e256c48241a07
abrusu-collins <snillochemingson@gmail.com>
2022-10-25 11:08:58 +0000

    I'm a genius

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -14,50 +14,47 @@ function Services() {
             <img src={web} alt="web-img" />
             <p className="heading">Website Development</p>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
-              incidunt corrupti dolore quisquam laudantium. Dolorem corporis
-              magni quos nesciunt hic nobis fugiat, pariatur minima ea eos eum
-              deserunt nihil totam!
+              We create and distribute safe websites. Our team design with
+              scalability in mind, utilizing modern technologies and
+              battle-tested software solutions.
             </p>
           </div>
           <div className="desktop">
             <img src={desktop} alt="desktop-img" />
             <p className="heading">Desktop App Development</p>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
-              incidunt corrupti dolore quisquam laudantium. Dolorem corporis
-              magni quos nesciunt hic nobis fugiat, pariatur minima ea eos eum
-              deserunt nihil totam!
+              We create custom desktop applications that are designed to meet
+              business challenges, promote growth, and achieve your business
+              goals.
             </p>
           </div>
           <div className="android">
             <img src={android} alt="android-img" />
             <p className="heading">Android App Development</p>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
-              incidunt corrupti dolore quisquam laudantium. Dolorem corporis
-              magni quos nesciunt hic nobis fugiat, pariatur minima ea eos eum
-              deserunt nihil totam!
+              Since almost everyone on the planet owns a smartphone, it is
+              advised that your business should have an android app. We have the
+              right developers to help you.
             </p>
           </div>
+        </div>
+        <div className="list-of-services2">
           <div className="uiux">
             <img src={uiux} alt="uiux-img" />
             <p className="heading">UI/UX Improvements</p>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
-              incidunt corrupti dolore quisquam laudantium. Dolorem corporis
-              magni quos nesciunt hic nobis fugiat, pariatur minima ea eos eum
-              deserunt nihil totam!
+              Do you want to enhance and beautify your website and strengthen
+              your business? We will work with you to pinpoint the issues and
+              develop a better experience for your customers.
             </p>
           </div>
           <div className="bug">
             <img src={bug} alt="bug-img" />
             <p className="heading">Bug fixing</p>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
-              incidunt corrupti dolore quisquam laudantium. Dolorem corporis
-              magni quos nesciunt hic nobis fugiat, pariatur minima ea eos eum
-              deserunt nihil totam!
+              We will offer you end-to-end bug fix solution as well as support
+              for your website. We have worked on a lot of products using a lot
+              of modern development tools so we know exacty where your bug is.
             </p>
           </div>
         </div>
