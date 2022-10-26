a57b0db01ef2ee1518f84d5043bdf908a0c2de74
abrusu-collins <snillochemingson@gmail.com>
2022-10-26 09:38:50 +0000

    left with blacko

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,11 +10,11 @@ function Hero() {
             DevTech is a sofware development agency in Accra, Ghana. We help you
             own quality and affordable digital products easily. We develop
             <span> Websites</span>, <span>Android Apps</span>, and
-            <span> Desktop Apps</span>. We also do <span>bug fixing</span> and{" "}
+            <span> Desktop Apps</span>. We also do <span>bug fixing</span> and
             <span>UI/UX improvements</span>.
           </p>
           <p className="discuss">Let's discuss your project.</p>
-          <a href=" ">Talk To Us Now</a>
+          <a href="mailto:snillochemingson@gmail.com">Talk To Us Now</a>
         </div>
         <div className="hero-right">
           <img src={heroimg} alt="heroimg" />
--- src/components/Ready.jsx
+++ src/components/Ready.jsx
@@ -1,12 +1,12 @@
 function Ready() {
-    return ( 
-        <div className="ready">
-            <div>
-            <p>Ready to discuss your project?</p>
-            <a href=" ">Talk To Us Now</a>
-            </div>
-        </div>
-    );
+  return (
+    <div className="ready">
+      <div>
+        <p>Ready to discuss your project?</p>
+        <a href="mailto:snillochemingson@gmail.com">Talk To Us Now</a>
+      </div>
+    </div>
+  );
 }
 
 export default Ready;
